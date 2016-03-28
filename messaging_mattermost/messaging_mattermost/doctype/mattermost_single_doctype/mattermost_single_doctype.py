de838ef474a364e0d8e4fb9aa3a7f2e27d93dd72
Valeriy Osipov <valeriy.osipov@semilimes.com>
2016-03-28 15:19:57 +0000

    Fixed messages orderm and added authors of messages

--- messaging_mattermost/messaging_mattermost/doctype/mattermost_single_doctype/mattermost_single_doctype.py
+++ messaging_mattermost/messaging_mattermost/doctype/mattermost_single_doctype/mattermost_single_doctype.py
@@ -7,40 +7,44 @@ import frappe
 from frappe.model.document import Document
 import frappe.email.mattermost
 
-# TODO; Отсортировать отображаемые сообщения по времени
-# TODO: investigating add path to mettermost module to PYTHONPATH. Fix $PATH variable
-# TODO: integrate Mattermost user accounts with ERPNext user accounts
+
 # TODO: Получение настроек подключения вынести в конструктор
+# TODO: переделать функцию получения сообщений на messages()
+# TODO: сделать получение списка сообщений при старте формы. А также polling для обновления списка сообщений
 
 class MattermostSingleDocType(Document):
-	pass
+    pass
+
 
 @frappe.whitelist()
 def send_message(message):
-	settings = frappe.get_doc("Mattermost Settings", "Notifications")
-	url = settings.url
-	team = settings.team
-	email = settings.email
-	password = settings.password
-	client = frappe.email.mattermost.MattermostClient(url, team, email, password)
+    settings = frappe.get_doc("Mattermost Settings", "Notifications")
+    url = settings.url
+    team = settings.team
+    email = settings.email
+    password = settings.password
+    client = frappe.email.mattermost.MattermostClient(url, team, email, password)
 
-	# post message in a channel
-	channels = client.api.get_channels()
-	channel = channels[1]['id']
-	client.channel_msg(channel, message)
+    # post message in a channel
+    channels = client.api.get_channels()
+    channel = channels[1]['id']
+    client.channel_msg(channel, message)
+
 
 @frappe.whitelist()
 def get_message_list():
-	settings = frappe.get_doc("Mattermost Settings", "Notifications")
-	url = settings.url
-	team = settings.team
-	email = settings.email
-	password = settings.password
-	client = frappe.email.mattermost.MattermostClient(url, team, email, password)
+    settings = frappe.get_doc("Mattermost Settings", "Notifications")
+    url = settings.url
+    team = settings.team
+    email = settings.email
+    password = settings.password
+    client = frappe.email.mattermost.MattermostClient(url, team, email, password)
 
-	channels = client.api.get_channels()
-	channel = channels[1]['id']
+    channels = client.api.get_channels()
+    channel = channels[1]['id']
 
-	messages = client.api.get_channel_posts(channel, 0)
-	message_list = [messages['posts'][m]['message'] for m in messages['posts']]
-	return '\n'.join(message_list)
+    users = client.get_users()
+    messages = client.api.get_channel_posts(channel, 0)
+    posts = sorted([(users[m['user_id']]['username'], m['create_at'], m['message']) for m in messages['posts'].values()], key=lambda x: x[1])
+
+    return '\n'.join(map(lambda x: '%s: %s' % (x[0], x[2]), posts))
